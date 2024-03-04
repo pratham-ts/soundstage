832c0711e5989e0fe65b3d597bd3d8de85b97e4f
pratham-ts <pratham.thakkar@talentsystems.com>
2024-03-04 12:53:12 +0530

    config module setup done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SoundstageModule } from './soundstage/soundstage.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import ormConfig from 'common/config/ormconfig';
 import { tblUsers } from './soundstage/entities/TblUsers';
 
+const ENV = process.env.NODE_ENV;
+
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: ENV ? `.env.${ENV}` : '.env' }),
     TypeOrmModule.forRoot({ ...ormConfig, entities: [tblUsers] }),
     SoundstageModule,
   ],
